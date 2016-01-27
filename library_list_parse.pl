197cfa75737b4e80de9fda5dcf913cd5ac3273f7
Ben Lind <mail@benlind.com>
2016-01-27 16:21:59 -0500

    Moved documentation to README

--- library_list_parse.pl
+++ library_list_parse.pl
@@ -19,47 +19,7 @@ $file defaults to 'export.txt'.
 
 =head1 DESCRIPTION
 
-This script takes an exported eiNetwork Library Reading History list file and
-outputs each record's title, author, and publisher. It separates each field with
-a tab and each record with a newline.
-
-=head1 EXAMPLE
-
-This script would take the following file:
-
-=begin text
-
-Record 1 of 3
-AUTHOR       Dirr, Michael.
-TITLE        Viburnums : flowering shrubs for every season / Michael A. Dirr ;
-               illustrations by Bonnie L. Dirr.
-PUBLISHER    Portland, Or. : Timber Press, ©2007.
-
-Record 2 of 3
-AUTHOR       Cushnie, John.
-TITLE        Shrubs for the garden / John Cushnie ; photography by Marianne 
-               Majerus.
-PUBLISHER    London : Kyle Cathie, ©2004.
-
-Record 3 of 3
-AUTHOR       Dickens, Charles, 1812-1870.
-TITLE        David Copperfield / Charles Dickens ; with an introduction by 
-               Paul Bailey.
-PUBLISHER    Oxford ; New York : Oxford University Press, 1999.
-
-=end text
-
-It would output the following content:
-
-=begin text
-
-Viburnums : flowering shrubs for every season / Michael A. Dirr ; illustrations by Bonnie L. Dirr.	Dirr, Michael.	Portland, Or. : Timber Press, ©2007.
-Shrubs for the garden / John Cushnie ; photography by Marianne Majerus.	Cushnie, John.	London : Kyle Cathie, ©2004.
-David Copperfield / Charles Dickens ; with an introduction by Paul Bailey.	Dickens, Charles, 1812-1870.	Oxford ; New York : Oxford University Press, 1999.
-
-=end text
-
-This file can then be imported into Excel with tabs as delimiters.
+See README.md for full description.
 
 =cut
 
