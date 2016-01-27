f8a2bcd56fe0230aa5b249c0be1daf48ef97b7b0
Ben Lind <mail@benlind.com>
2016-01-27 16:03:26 -0500

    Add script

---
+++ library_list_parse.pl
@@ -0,0 +1,123 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use autodie;
+use feature ':5.10';
+
+=pod
+
+=head1 NAME
+
+library_list_parse.pl - Parses an exported eiNetwork Library Reading History
+file into tab-delimited output.
+
+=head1 SYNOPSIS
+
+perl library_list_parse.pl [file]
+
+$file defaults to 'export.txt'.
+
+=head1 DESCRIPTION
+
+This script takes an exported eiNetwork Library Reading History list file and
+outputs each record's title, author, and publisher. It separates each field with
+a tab and each record with a newline.
+
+=head1 EXAMPLE
+
+This script would take the following file:
+
+=begin text
+
+Record 1 of 3
+AUTHOR       Dirr, Michael.
+TITLE        Viburnums : flowering shrubs for every season / Michael A. Dirr ;
+               illustrations by Bonnie L. Dirr.
+PUBLISHER    Portland, Or. : Timber Press, ©2007.
+
+Record 2 of 3
+AUTHOR       Cushnie, John.
+TITLE        Shrubs for the garden / John Cushnie ; photography by Marianne 
+               Majerus.
+PUBLISHER    London : Kyle Cathie, ©2004.
+
+Record 3 of 3
+AUTHOR       Dickens, Charles, 1812-1870.
+TITLE        David Copperfield / Charles Dickens ; with an introduction by 
+               Paul Bailey.
+PUBLISHER    Oxford ; New York : Oxford University Press, 1999.
+
+=end text
+
+It would output the following content:
+
+=begin text
+
+Viburnums : flowering shrubs for every season / Michael A. Dirr ; illustrations by Bonnie L. Dirr.	Dirr, Michael.	Portland, Or. : Timber Press, ©2007.
+Shrubs for the garden / John Cushnie ; photography by Marianne Majerus.	Cushnie, John.	London : Kyle Cathie, ©2004.
+David Copperfield / Charles Dickens ; with an introduction by Paul Bailey.	Dickens, Charles, 1812-1870.	Oxford ; New York : Oxford University Press, 1999.
+
+=end text
+
+This file can then be imported into Excel with tabs as delimiters.
+
+=cut
+
+my $filename = $ARGV[0] // 'export.txt';
+open my $file, '<', $filename;
+
+=head2 trim([$string1, [$string2, ...]])
+
+trim() removes whitespace from the beginning and end of any passed
+strings. NOTE: It modifies the passed variables IN-PLACE, changing their
+referenced values, not copies. If no strings are passed, it operates on $_, so
+that you can call `trim` without arguments.
+
+=cut
+sub trim { return @_ ? do { s/^\s+|\s+$//g for @_ } : s/^\s+|\s+$//g };
+
+my @records;
+my $i = 0;
+my $prev_title = 0; # was the previous line part of a title?
+while (<$file>) {
+    if (/^Record \d+ of /) {
+        $i++;
+        $records[$i] = {};
+    }
+    elsif (/^AUTHOR/) {
+        $_ =~ s/^AUTHOR\s+//;
+        trim;
+        $records[$i]->{author} = $_;
+    }
+    elsif (/^PUBLISHER/) {
+        $_ =~ s/^PUBLISHER\s+//;
+        trim;
+        $records[$i]->{publisher} = $_;
+    }
+    elsif (/^TITLE/) {
+        $_ =~ s/^TITLE\s+//;
+        trim;
+        $records[$i]->{title} = $_;
+        $prev_title = 1;
+    }
+    elsif ($prev_title && /^ /) {
+        # The previous line was part of a title, which wrapped to this line.
+        trim;
+        $records[$i]->{title} .= ' ' . $_;
+    }
+    else {
+        $prev_title = 0;
+    }
+}
+
+# Print the records, delimited by tabs.
+my $delim = "\t";
+for $i (1 .. $#records) {
+    my $title = $records[$i]{title} || '';
+    my $author = $records[$i]{author} || '';
+    my $publisher = $records[$i]{publisher} || '';
+    print $title . $delim;
+    print $author . $delim;
+    print $publisher . $delim;
+    print "\n";
+}
